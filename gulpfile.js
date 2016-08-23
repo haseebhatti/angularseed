3ef0587ea386e95746175fd7f5371e85706b1c8f
haseeb bhatti <haseebhatti@gmail.com>
2016-08-23 15:22:25 -0400

    added new files

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,17 @@
 // grab our gulp packages
 var gulp = require('gulp'),
   gutil = require('gulp-util');
+var sass = require('gulp-sass');
 
 // create a default task and just log a message
 gulp.task('default', function () {
   return gutil.log('Gulp is running!');
+});
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./app/css'));
+});
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
 });
